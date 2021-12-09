e7a863dd474af00985fcfa7557bb83630a1f3381
akaiqt882001234 <anhnpngcd191020@fpt.edu.vn>
2021-12-09 17:55:15 +0700

    Course Controller

---
+++ AppDev_Na/ViewModels/CourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using AppDev_Na.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AppDev_Na.ViewModels
+{
+    public class CourseViewModel 
+    {
+        // GET
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public Course Course { get; set; }
+    }
+}
